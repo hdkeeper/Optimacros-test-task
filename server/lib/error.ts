05ceb9858af25b19a91b84260864f6a009464b52
Keeper <hd_keeper@mail.ru>
2024-07-15 19:51:41 +0300

    Update error.ts

--- server/lib/error.ts
+++ server/lib/error.ts
@@ -1,6 +1,6 @@
 import { Response } from 'express';
 
-export const internalError = (res: Response, exception: any) => {
+export const internalError = (res: Response, exception: unknown) => {
     console.error(exception);
     res.status(500).send({ error: 'Internal error' });
 };
